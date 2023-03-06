3dfeb3fb014fc8ccbc989a7d3153b4e11618090a
J1min <aoiharu41@gmail.com>
2023-03-06 22:28:20 +0900

    스타일 주는 법 추가

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('앱임'),
-      ),
-      body: Text('안녕'),
-      bottomNavigationBar: BottomAppBar(
-          child: Container(
-        padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Icon(Icons.call),
-          Icon(Icons.message),
-          Icon(Icons.contact_page)
-        ]),
-      )),
-    ));
+            appBar: AppBar(
+              title: Text('앱임'),
+            ),
+            body: Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+              ),
+            )));
   }
 }
