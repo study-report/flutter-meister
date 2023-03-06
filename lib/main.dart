006caf2789e3907d9512ea1df36ce215806c7a16
J1min <aoiharu41@gmail.com>
2023-03-06 22:12:36 +0900

    flex 레이아웃 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,27 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Image.asset('img.png')
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('앱임'),
+      ),
+      body: Text('안녕'),
+      bottomNavigationBar: BottomAppBar(
+          child: Container(
+        padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Icon(Icons.call),
+          Icon(Icons.message),
+          Icon(Icons.contact_page)
+        ]),
+      )),
+    ));
   }
 }
