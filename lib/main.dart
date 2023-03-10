5382c4b93e1677482dd71e8c07f55b4df8c96f0c
J1min <aoiharu41@gmail.com>
2023-03-10 23:42:46 +0900

    플러터 레이아웃 공부 추가2

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('제목입니다'),
-            ),
-            body: Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                ),
-              ),
-            )));
+          appBar: titleSection,
+          body: bodySection,
+        ));
   }
+
+  PreferredSizeWidget titleSection = AppBar(
+    title: Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [Text('플러ㅗ터 레이아ㅓ웃 데모')],
+    )),
+  );
+
+  Widget bodySection = Container(
+      padding: EdgeInsets.all(40),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "ohchard",
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 5),
+                    child: Text(
+                      'ddd',
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  )
+                ],
+              ),
+              Row(children: [Icon(Icons.star, color: Colors.red), Text('41')])
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(children: [
+                  Icon(Icons.ac_unit),
+                  Container(
+                    child: Text('CALL'),
+                    padding: EdgeInsets.only(top: 10),
+                  )
+                ]),
+                Column(children: [
+                  Icon(Icons.ac_unit),
+                  Container(
+                    child: Text('CALL'),
+                    padding: EdgeInsets.only(top: 10),
+                  )
+                ]),
+                Column(children: [
+                  Icon(Icons.ac_unit),
+                  Container(
+                    child: Text('CALL'),
+                    padding: EdgeInsets.only(top: 10),
+                  )
+                ]),
+              ],
+            ),
+          )
+        ],
+      ));
 }
